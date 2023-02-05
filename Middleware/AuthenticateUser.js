8be65b833ccc602a524336ad340abca45ce4b01a
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2023-02-04 20:39:50 -0500

    user get resolved

--- Middleware/AuthenticateUser.js
+++ Middleware/AuthenticateUser.js
@@ -3,14 +3,31 @@ const User = require('../Controllers/UserController');
 
 const authenticateUser = async (req, res, next) => {
     
+    let userType = req.type
+    let userFound = false
 
-    let userFound = await User.getUserWithEmailAndPassword() 
+    if(!req.email || !req.password || !req.type){
+      return res.status(400).json({
+        status: false,
+        message : "Please send valid data to acces API."
+      })
+      
+    }
+    if (userType == "COMPANY") {
+      // userFound = await Compan
+      // add company model ahere
+    }else {
+      userFound = await User.getUserWithEmailAndPassword() 
+    }
     
-      userFound ? next() : res.status(401).json({
+    
+    userFound ? next() : res.status(401).json({
         status: false, message: "You are not authorized"
     })
       
     
   };
+
+  
 
   module.exports = authenticateUser
